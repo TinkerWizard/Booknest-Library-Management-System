46480f7a3c51b5f780d0634cb07c47873f67734b
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-04-11 09:35:33 +0530

    deleted the basic version and added spring

--- src/resources/resources.java
+++
@@ -1,5 +0,0 @@
-package src.resources;
-
-public class resources {
-    
-}
---
+++ src/test/java/com/library/booknest/BooknestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.booknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
