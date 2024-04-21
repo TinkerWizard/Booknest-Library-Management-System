ebbd9f016d397eb0298bf09d4736ee1153a5dcd1
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-04-21 19:49:37 +0530

    Added sign in auth and organized methods wrt the user type

--- src/main/java/lms/Account.java
+++ src/main/java/lms/Account.java
@@ -1,5 +1,102 @@
 package lms;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Account {
+    public String starter(Connection connection) {
+        try
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String username = "";
+            System.out.println("1. Sign Up");
+            System.out.println("2. Sign In");
+            System.out.println("Choose yours: ");
+            String choiceStr = reader.readLine();
+            int choice = Integer.parseInt(choiceStr);
+            if (choice == 1) {
+                username = signUp();
     
+            } else if (choice == 2) {
+                // show options
+                username = signIn(connection);
+            } else {
+                System.out.println("Exiting...!");
+            }
+            return username;
+        }
+        catch(IOException e)
+        {
+            System.out.println("An error occurred while reading input: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public static String signUp() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String username = reader.readLine();
+            String password = reader.readLine();
+            String user_type = "";
+            String name = reader.readLine();
+            String email = reader.readLine();
+            return username;
+        } catch (Exception e) {
+            System.out.println("An error occurred while reading input: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public static String signIn( Connection connection ) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter username:");
+            String username = reader.readLine();
+            System.out.println("Enter password:");
+            String password = reader.readLine();
+            try {
+                // Construct the SQL query with a prepared statement
+                String query = "SELECT * FROM \"User\" WHERE username = ?";
+    
+                PreparedStatement pstmt = connection.prepareStatement(query);
+                pstmt.setString(1, username);
+    
+                // Execute the query
+                ResultSet rs = pstmt.executeQuery();
+    
+                if (rs.next()) {
+                    // User found, now check the password
+                    String storedPassword = rs.getString("password");
+                    if (password.equals(storedPassword)) {
+                        // Passwords match, user authenticated
+                        System.out.println("User authenticated!");
+                        return username;
+                    } else {
+                        // Passwords don't match
+                        System.out.println("Incorrect password!");
+                    }
+                } else {
+                    // User not found
+                    System.out.println("User not found!");
+                }
+                // Close the result set and prepared statement
+                rs.close();
+                pstmt.close();
+                return "";
+            } catch (SQLException e) {
+                // Handle any SQL errors
+                e.printStackTrace();
+                return "";
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred while reading input: " + e.getMessage());
+            return "";
+        }
+    }
 }
--- src/main/java/lms/LMS.java
+++ src/main/java/lms/LMS.java
@@ -4,6 +4,7 @@ import java.sql.Statement;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -21,55 +22,85 @@ public class LMS {
             Statement st = con.createStatement();
             LibraryDatabase libraryDatabase = new LibraryDatabase();
             Librarian librarian = new Librarian();
-
+            Account account = new Account();
             Scanner scanner = new Scanner(System.in);
+            String currentUsername = account.starter(con);
+            System.out.println("The current username is: " + currentUsername);
+            String query = "SELECT user_type FROM \"User\" WHERE username = '" + currentUsername +"'";
+            st.executeQuery(query);
+            ResultSet rs = st.executeQuery(query);
 
-            int choice;
+            String userType = "";
+            while (rs.next()) {
+                userType = rs.getString("user_type");
+            }
+            System.out.println("The userType: " + userType);
+            int choice = 0;
             do {
-                System.out.println("\nLibrary Management System");
-                System.out.println("1. Display all books");
-                System.out.println("2. Search for a book");
-                System.out.println("3. Borrow a book");
-                System.out.println("4. Add a book");
-                System.out.println("5. Display students");
-                System.out.println("6. Display transactions");
-                System.out.println("7. Exit");
-                System.out.print("Enter your choice: ");
-                choice = scanner.nextInt();
-                scanner.nextLine(); // Consume newline character
+                if (userType.equals("Librarian")) {
+                    System.out.println("\nLibrary Management System");
+                    System.out.println("1. Display all books");
+                    System.out.println("2. Search for a book");
+                    System.out.println("3. Add a book");
+                    System.out.println("4. Display students");
+                    System.out.println("5. Display transactions");
+                    System.out.println("6. Exit");
+                    System.out.print("Enter your choice: ");
+                    choice = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline character
+                    switch (choice) {
+                        case 1:
+                            System.out.println("\nAll Books:");
+                            libraryDatabase.displayBooks(con);
+                            break;
+                        case 2:
+                            System.out.print("Enter title or author to search: ");
+                            String keyword = scanner.nextLine();
+                            System.out.println("\nSearch Results:");
+                            libraryDatabase.searchBook(keyword, con);
+                            break;
+                        case 3:
+                            libraryDatabase.addBook(st);
+                            break;
+                        case 4:
+                            librarian.displayStudents(con, st);
+                            break;
+                        case 5:
+                            librarian.displayTransaction(con, st);
+                            break;
+                        case 6:
+                            System.out.println("Exiting...");
+                            break;
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
 
-                switch (choice) {
-                    case 1:
-                        System.out.println("\nAll Books:");
-                        libraryDatabase.displayBooks(con);
-                        break;
-                    case 2:
-                        System.out.print("Enter title or author to search: ");
-                        String keyword = scanner.nextLine();
-                        System.out.println("\nSearch Results:");
-                        libraryDatabase.searchBook(keyword, con);
-                        break;
-                    case 3:
-                        System.out.print("Enter title or author to borrow: ");
-                        String borrowKeyword = scanner.nextLine();
-                        libraryDatabase.borrowBooks(borrowKeyword);
-                        break;
-                    case 4:
-                        libraryDatabase.addBook(st);
-                        break;
-                    case 5:
-                        librarian.displayStudents(con, st);
-                        break;
-                    case 6:
-                        librarian.displayTransaction(con, st);
-                    case 7:
-                        System.out.println("Exiting...");
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please try again.");
+                    }
+                } if (userType.equals("Student")) {
+                    System.out.println("\nLibrary Management System");
+                    System.out.println("1. Borrow a book");
+                    System.out.println("2. Return a book");
+                    System.out.println("6. Exit");
+                    choice = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline character
+                    switch (choice) {
+                        case 1:
+                            System.out.print("Enter title or author to borrow: ");
+                            String borrowKeyword = scanner.nextLine();
+                            libraryDatabase.borrowBooks(borrowKeyword);
+                            break;
+                        case 2:
+                            System.out.println("Enter the book id to return");
+                            break;
+                        case 6:
+                            System.out.println("Exiting...");
+                            break;
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                    }
                 }
-            } while (choice != 7);
+            } while (choice != 6);
             scanner.close();
+            con.close();
         } catch (SQLException e) {
             System.out.println(e);
         }
